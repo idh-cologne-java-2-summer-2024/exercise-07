4df8faf46cdc880ba8aa33515ddd513cfe413bf1
limpl <limpl@DESKTOP-NCMTBB0.fritz.box>
2024-06-05 20:01:54 +0200

    1

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,18 +1,30 @@
 package idh.java;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.HashMap; 
+
+
 
 public class Bank implements Iterable<Account> {
 	Account[] accounts = new Account[5];
+	
+	
+	
+	HashMap <Integer, Account> bankAccounts = new HashMap <Integer, Account>();
 
 	public Bank() {
-		// create accounts with varying balances
+		
+		// create accounts with varying balanceMap
 		Random random = new Random();
 		for (int i = 0; i < accounts.length; i++) {
 			accounts[i] = new Account(i, random.nextInt(1000));
+			//bankAccounts.put(random.nextInt(1000), Account a);
+			
 		}
+		
 	}
 	
 	@Override
@@ -20,9 +32,73 @@ public class Bank implements Iterable<Account> {
 		return new AccountIterator(accounts);
 	}
 	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
-	}
 
+
+	public static void main (String[] args) {
+		
+		
+		
+	}
+	
+
+	
+	
+	
+	public class HashMap <Integer, Account>{
+		
+		
+		List[]buckets;
+		
+		
+		public HashMap() {
+		buckets = new List [5];
+		}
+		
+		public Account getAccount (Integer accountnummer) {
+			int hc = accountnummer.hashCode();
+			int bucketIndex = hc % buckets.length;
+			
+			List<Node>list = buckets[bucketIndex];
+			for (Node node : list) {
+			 if (node.accountnummer.equals(accountnummer))	{
+			return node.account; 	 
+				 
+			 }
+				
+				
+			}
+			
+			return null;
+		}
+		
+		class Node {
+		Integer accountnummer; 
+		Account account; 
+			
+		}
+		
+	
+		
+		
+		
+		
+		public void put(Integer accountnummer, Account accountbla) {
+		int hc = accountnummer.hashCode();
+		int bucketIndex = hc % buckets.length; 	
+		
+	
+		
+		Node node = new Node();
+		node.accountnummer = accountnummer; 
+		node.account = getAccount(null) ; 
+		}
+		
+		
+	}
+	
+	
+
+	
 }
+
+
