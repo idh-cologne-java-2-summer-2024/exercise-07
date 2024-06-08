f1e07be6d9fcd4e953ca76daf128ae4b99525e96
Yuima <Yuima@DwieDosenlaptop.local>
2024-06-08 16:29:15 +0200

    Aufgabe 1 gelöst <- also es klappt irgendwie

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,38 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
+	//[] accounts = new Account[5];
+	HashMap<Integer,Account> accountsMap = new HashMap <Integer, Account>();
 
 	public Bank() {
 		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+		for (int i = 0; i < 5; i++) {
+			Account currentAccount = new Account(i, 500);
+			addAccount(currentAccount);
 		}
+	}
+	/**
+	 * Function to add an account to the hash-map*/
+	protected HashMap addAccount(Account account) {
+		accountsMap.put(account.id, account);
+		
+		return accountsMap;
 	}
 	
 	@Override
 	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
+		
+		return null;
 	}
 	
 	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+		
+		return accountsMap.get(number);
 	}
 
 }
