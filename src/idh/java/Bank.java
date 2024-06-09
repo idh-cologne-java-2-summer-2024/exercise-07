70602e5ac5bea17363466677886ceefef46788b2
Kirby <Kirby@LAPTOP-TBIH83MV.speedport.ip>
2024-06-09 21:56:43 +0200

    Exercise 07 Emoji als Banknummer_Sh-Klein

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,34 @@
 package idh.java;
 
-
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
+    private Account[] accounts;
+    private HashMap<String, Account> accountMap = new HashMap<>();
 
-	public Bank() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
-		}
-	}
-	
-	@Override
-	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
-	}
-	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
-	}
+    public Bank() {
+        // Initialize accounts with some example account numbers and balances
+        accounts = new Account[] {
+            new Account("1234@5678", new Random().nextInt(1000)),
+            new Account("cat_haz_cash", new Random().nextInt(1000)),
+            new Account("💸", new Random().nextInt(1000)),
+            new Account("123", new Random().nextInt(1000)),
+            new Account("abc123", new Random().nextInt(1000))
+        };
 
+        for (Account account : accounts) {
+            accountMap.put(account.getId(), account);
+        }
+    }
+
+    @Override
+    public Iterator<Account> iterator() {
+        return new AccountIterator(accounts);
+    }
+
+    public Account getAccount(String number) {
+        return accountMap.get(number);
+    }
 }
