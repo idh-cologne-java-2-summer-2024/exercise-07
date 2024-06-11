4e7cea0454367691cab89f8276c33455746e8503
Marc Tetsuo Benscheid <mbensche@smail.de>
2024-06-11 16:38:57 +0200

    - initial commit

--- src/idh/java/AccountIterator.java
+++ src/idh/java/AccountIterator.java
@@ -1,24 +1,34 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 class AccountIterator implements Iterator<Account> {
 	int currentPosition = 0;
-	Account[] accounts;
+//	Account[] accounts;
+	HashMap <Integer,Account>accounts = new LinkedHashMap<Integer,Account>();
 	
-	public AccountIterator(Account[] accounts) {
+	public AccountIterator(HashMap<Integer, Account> accounts) {
 		this.accounts = accounts;
 	}
 	
 	@Override
 	public boolean hasNext() {
-		return currentPosition < accounts.length;
+		return currentPosition < accounts.size();
 	}
 
 	@Override
 	public Account next() {
-		return accounts[currentPosition++];
+//		TODO: Implement
+//		Map.Entry<Integer, Account> next;
+//		return next.getValue();
+		
+//		return accounts[currentPosition++];
+		return null;
 	}
 	
 }
--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,38 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Random;
 
-public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
+//public class Bank implements Iterable<Account> {
+	public class Bank {
+	//HashMap replace Array
+	HashMap <String,Account>accounts = new HashMap<String,Account>();
 
 	public Bank() {
 		// create accounts with varying balances
 		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+		int i = 0;
+		String aId = 0+"";
+		while (i < 10) {
+			accounts.put( ""+i, new Account(aId, random.nextInt(1000)));
+			i++;
 		}
 	}
 	
-	@Override
-	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
-	}
-	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+//	@Override
+//	public Iterator<Account> iterator() {
+//		return new AccountIterator(accounts);
+//	}
+	/**
+	 * Returns an Account object of an account
+	 * @param number
+	 * @return The Account belonging to the account number
+	 */
+	public Account getAccount(String number) {
+		return accounts.get(number);
 	}
 
 }
