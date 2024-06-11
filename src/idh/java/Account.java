6e5b3f4b2a4d10724dd6aff429f432b52831965e
DaveSanti <dgolomb94@gmail.com>
2024-06-11 14:44:55 +0200

    Excercise 1 & 2

--- src/idh/java/Account.java
+++ src/idh/java/Account.java
@@ -1,46 +1,31 @@
 package idh.java;
 
-
-/**
- * This class represents an account in our bank.
- * @author reiterns
- *
- */
 public class Account {
-	// the balance of the account
-	int balance;
-	
-	// the id of the account
-	int id;
+    int balance;
+    String id;
 
-	public Account(int id, int status) {
-		this.id = id;
-		this.balance = status;
-	}
-	
-	public int getId() {
-		return id;
-	}
+    public Account(String id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public int getBalance() {
-		return balance;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setBalance(int status) {
-		this.balance = status;
-	}
-	
-	/**
-	 * Withdraws a sum of money from the account
-	 * @param sum
-	 */
-	public void withdraw(int sum) {
-		this.balance = balance - sum;
-	}
-	
-	
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int sum) {
+        this.balance -= sum;
+    }
 }
--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,31 @@
 package idh.java;
 
-
 import java.util.Iterator;
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
+    private Map<String, Account> accountMap = new HashMap<>();
 
-	public Bank() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
-		}
-	}
-	
-	@Override
-	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
-	}
-	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
-	}
+    public Bank() {
+    }
 
+    @Override
+    public Iterator<Account> iterator() {
+        return accountMap.values().iterator();
+    }
+
+    public Account getAccount(String number) {
+        return accountMap.get(number);
+    }
+
+    public void createAccount(String id, int balance) {
+        if (accountMap.containsKey(id)) {
+            System.out.println("Account ID already exists. Please choose a different ID.");
+        } else {
+            Account newAccount = new Account(id, balance);
+            accountMap.put(id, newAccount);
+            System.out.println("Account created successfully.");
+        }
+    }
 }
