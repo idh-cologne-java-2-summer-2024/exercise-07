8a436ca20d89f04711d414b7def28645a9869ed5
dejag <dejag@Daichi>
2024-06-11 10:59:50 +0200

    Exercise 7

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,90 +1,47 @@
 package idh.java;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM  {
-	
-	// initial cash in the ATM
 	int cash = 100;
-
 	Bank bank;
-	
 	public ATM(Bank bank) {
-		this.bank = bank;
-	}
+		this.bank = bank;}
 	
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
 				System.out.print("Enter your account number: ");
-				int accountNumber = Integer.parseInt(br.readLine());
+				String accountNumber = br.readLine();
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(accountNumber, amount);
 			} catch (Exception e) {
 				e.printStackTrace();
-				break;
-			}
-		}
-	}
+				break;}}}
 
-	public void cashout(int accountNumber, int amount) {
-		// check for cash in the ATM
+	public void cashout(String accountNumber, int amount) {
 		if (amount > cash) {
 			System.out.println("Sorry, not enough cash left.");
-			return;
-		}
+			return;}
 		
-		// check for existence of the account
-		Account account = getAccount(accountNumber);
+		Account account = bank.getAccount(accountNumber);
 		if (account == null) {
 			System.out.println("Sorry, this account doesn't exist.");
-			return;
-		}
-		
-		// check for balance of the account
+			return;}
 		if (amount > account.getBalance()) {
 			System.out.println("Sorry, you're out of money.");
-			return;
-		}
+			return;}
 		
-		// withdraw
 		account.withdraw(amount);
 		cash += amount;
-		System.out.println("Ok, here is your money, enjoy!");
+		System.out.println("Ok, here is your money, enjoy!");};
 
-	};
-
-	/**
-	 * Launches the ATM
-	 */
 	public static void main(String[] args) {
 		Bank bank = new Bank();
 		ATM atm = new ATM(bank);
-		atm.run();
-	};
+		atm.run();};
 	
-	/**
-	 * Retrieves the account given an id.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	protected Account getAccount(int id) {
-		for (Account account : bank) {
-			if (account.getId() == id) 
-				return account;
-		}
-		return null;
-	}
-
-}
+	protected Account getAccount(String id) {
+		return bank.getAccount(id);}}
--- src/idh/java/Account.java
+++ src/idh/java/Account.java
@@ -1,46 +1,18 @@
 package idh.java;
 
-
-/**
- * This class represents an account in our bank.
- * @author reiterns
- *
- */
 public class Account {
-	// the balance of the account
 	int balance;
-	
-	// the id of the account
-	int id;
-
-	public Account(int id, int status) {
+	String id;
+	public Account(String id, int status) {
 		this.id = id;
-		this.balance = status;
-	}
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+		this.balance = status;}
+	public String getId() {
+		return id;}
+	public void setId(String id) {
+		this.id = id;}
 	public int getBalance() {
-		return balance;
-	}
-
+		return balance;}
 	public void setBalance(int status) {
-		this.balance = status;
-	}
-	
-	/**
-	 * Withdraws a sum of money from the account
-	 * @param sum
-	 */
+		this.balance = status;}
 	public void withdraw(int sum) {
-		this.balance = balance - sum;
-	}
-	
-	
-}
+		this.balance = balance - sum;}}
