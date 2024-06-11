e384ea4326c4690aa74c7b968a9111cb08c50c24
leoni <leoni@Leonies_Laptop.fritz.box>
2024-06-11 11:01:00 +0200

    ha

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,34 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
-public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
-
+public class Bank {
+	//      Account ID ---> Account         key,  Value
+    HashMap <String,Account>  accounts= new HashMap<String,Account>();
+	
 	public Bank() {
 		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
-		}
+		//Random random = new Random();
+		//for (int i = 0; i < 10; i++) {
+			
+		//	accounts.put(""+i,new Account(i,random.nextInt(1000)) ); // aus  dem int wird ein string 
+			//accounts[i] = new Account(i, random.nextInt(1000));
+		//}
+		accounts.put("meinAccount", new Account("meinAccount",100));
 	}
 	
-	@Override
-	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
-	}
+	//@Override
+	//public Iterator<Account> iterator() {
+	//	return new AccountIterator(accounts);
+	//}
 	
-	public Account getAccount(int number) {
+	
+	public Account getAccount(String number) {
 		// TODO: Implement
-		return null;
+		return accounts.get(number);
 	}
 
 }
