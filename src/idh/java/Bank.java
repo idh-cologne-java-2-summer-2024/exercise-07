b0bb22eb3c5f764f3f4ba7b9aac35cd779d732c0
smillaurban <smillaurban@macbook-air-von-smilla-2.fritz.box>
2024-06-11 10:50:55 +0200

    Aufgbe2

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -9,16 +9,10 @@ public class Bank {
 	
 	HashMap<String, Account> accounts= new HashMap<String, Account>();
 
+	
+	
 	public Bank() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < 10; i++) {
-			
-			accounts.put(""+i, new Account(i, random.nextInt(1000)));
-			
-		}
-		
-		accounts.get("halloooo");
+		accounts.put("Hallo", new Account("Hallo", 200));
 	}
 	
 	
