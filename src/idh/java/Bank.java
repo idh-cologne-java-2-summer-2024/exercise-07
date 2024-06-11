e24c4d33e75213ab6afef6a1a54d18e528521c8d
ginkogruen <jasper@wolter-koeln.de>
2024-06-11 23:03:57 +0200

    fix: removed some unnecessary comments

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -6,17 +6,8 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Bank {
-	//Account[] accounts = new Account[5];
 	HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
 
-	//public Bank() {
-	//	// create accounts with varying balances
-	//	Random random = new Random();
-	//	for (int i = 0; i < accounts.length; i++) {
-	//		accounts[i] = new Account(i, random.nextInt(1000));
-	//	}
-	//}
-	
 	public Bank() {
 		// Generate 10 accounts with random balances for testing purposes
 		Random random = new Random();
@@ -31,5 +22,4 @@ public class Bank {
 		Account wantedAccount = accounts.get(number);
 		return wantedAccount;
 	}
-
 }
