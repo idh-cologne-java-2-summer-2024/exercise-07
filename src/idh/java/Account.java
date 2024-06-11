8e04a45e4275db5a1224e72d846eec60149565f2
Fana <Fana@DESKTOP-ARPHR48.fritz.box>
2024-06-11 22:01:45 +0200

    Exercise 7 / Aufgabe 1 & 2 / FanaFanaFana

--- src/idh/java/Account.java
+++ src/idh/java/Account.java
@@ -11,18 +11,18 @@ public class Account {
 	int balance;
 	
 	// the id of the account
-	int id;
+	String id;
 
-	public Account(int id, int status) {
+	public Account(String id, int status) {
 		this.id = id;
 		this.balance = status;
 	}
 	
-	public int getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
@@ -41,6 +41,8 @@ public class Account {
 	public void withdraw(int sum) {
 		this.balance = balance - sum;
 	}
+
+	
 	
 	
 }
--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -3,26 +3,30 @@ package idh.java;
 
 import java.util.Iterator;
 import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bank implements Iterable<Account> {
 	Account[] accounts = new Account[5];
+    private Map<String, Account> accountMap = new HashMap<>();
 
-	public Bank() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
-		}
-	}
+
+    public Bank() {
+    	String[] accountNumbers = {"1234@5678", "cat_haz_cash", "💸", "user123", "guest456"};
+        Random random = new Random();
+        for (int i = 0; i < accounts.length; i++) {
+            accounts[i] = new Account(accountNumbers[i], random.nextInt(1000));
+            accountMap.put(accounts[i].getId(), accounts[i]);
+        }
+    }
 	
 	@Override
 	public Iterator<Account> iterator() {
 		return new AccountIterator(accounts);
 	}
 	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+	public Account getAccount(String number) {
+	    return accountMap.get(number);
 	}
 
 }
