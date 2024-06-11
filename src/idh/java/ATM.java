be86d45b1145bbba1266ff7dd672ea57952b119c
lulax <lulax@DESKTOP-EJPB6H8.speedport.ip>
2024-06-11 20:37:43 +0200

    Aufgabe 1

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -3,6 +3,7 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ATM  {
 	
@@ -70,6 +71,7 @@ public class ATM  {
 	public static void main(String[] args) {
 		Bank bank = new Bank();
 		ATM atm = new ATM(bank);
+		System.out.println(Arrays.toString(bank.accounts));
 		atm.run();
 	};
 	
--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,18 +1,28 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Bank implements Iterable<Account> {
 	Account[] accounts = new Account[5];
+	HashMap<Integer, Account> accMap;
 
 	public Bank() {
-		// create accounts with varying balances
+
+		HashMap<Integer, Account> accMap = new HashMap<>();
+		
 		Random random = new Random();
+		
 		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+			accounts[i] = new Account(i, random.nextInt(1000));	
 		}
+		
+		for (int c = 0; c < accounts.length; c++) {
+			accMap.put(random.nextInt(1000), accounts[c]);
+		}
+		this.accMap = accMap;
 	}
 	
 	@Override
@@ -21,8 +31,12 @@ public class Bank implements Iterable<Account> {
 	}
 	
 	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+		if(accMap.containsKey(number)) {
+		return accMap.get(number);
+		} else {
+			System.out.println("account does not exist");
+			return null;
+		}
 	}
 
 }
