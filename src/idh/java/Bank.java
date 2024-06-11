179a6e36ad3daa99bf6428b1bc2c22eb8837ee61
ginkogruen <jasper@wolter-koeln.de>
2024-06-11 23:19:42 +0200

    feat: completed exercise 2

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,25 +1,26 @@
 package idh.java;
 
-
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Random;
 
 public class Bank {
-	HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
+	HashMap<String, Account> accounts = new HashMap<>();
 
 	public Bank() {
 		// Generate 10 accounts with random balances for testing purposes
 		Random random = new Random();
 		for (int i = 0; i < 10; i++) {
+			String accountString = "IchbineinRandomString" + i;
 			Account exampleAcc = new Account(i, random.nextInt(1000));
-			accounts.put(i, exampleAcc);
+			accounts.put(accountString, exampleAcc);
 		}
+			Account exampleAcc = new Account(11, random.nextInt(1000));
+			accounts.put("1234@5678", exampleAcc);
+			accounts.put("cat_haz_cash", exampleAcc);
+			accounts.put("💸", exampleAcc);
 	}
 	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		Account wantedAccount = accounts.get(number);
-		return wantedAccount;
+	public Account getAccount(String number) {
+	return accounts.get(number);
 	}
 }
